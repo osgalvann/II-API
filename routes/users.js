8130120f859ffda4b7f678240455fea9ea690e90
osgalvan <os.galvan.m@gmail.com>
2022-06-01 16:28:01 -0500

    added login function with id param

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 
 const{
     retrieveUsers,
+    userLogIn,
     userSignUp,
     editUser,
     deleteUser
 } = require('../controllers/user');
 router.get('/', retrieveUsers);
+router.get('/:id', userLogIn)
 router.post('/', userSignUp);
 router.put('/:id',editUser);
 router.delete('/:id',deleteUser);
