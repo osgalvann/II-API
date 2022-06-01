feb0631e0a4722029d8d8d18de39ebc9e9e08eae
Oscar Galvan <94877256+osgalvann@users.noreply.github.com>
2022-06-01 16:42:37 -0500

    Revert "added login function with id param"

--- controllers/user.js
+++ controllers/user.js
@@ -5,24 +5,6 @@ function retrieveUsers(req, res) {
         .then(users => res.status(200).send(users))
 }
 
-function userLogIn(req, res) {
-    let id = req.params.id;
-    let passw = req.body.Password;
-    User.findOne({
-        where: { IdUser: id }
-    })
-        .then(user => {
-            if (user.validatePassword(passw)) {
-                res.status(200).send(user)
-            }
-            else {
-                res.status(401).send("Unable to log in")
-            }
-        })
-
-
-}
-
 function userSignUp(req, res) {
     let body = req.body;
     User.create(body)
@@ -43,14 +25,13 @@ function editUser(req, res) {
 function deleteUser(req, res) {
     let id = req.params.id;
     User.destroy({
-        where: { IdUser: id }
+        where: {IdUser: id}
     })
         .then(r => res.status(200).send("Deleted"))
 }
 
 module.exports = {
     retrieveUsers,
-    userLogIn,
     userSignUp,
     editUser,
     deleteUser
